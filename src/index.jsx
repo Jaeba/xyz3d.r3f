5c4d71dfca9b549751601a2b77e52b459c58f6c5
Edmund Dao <61922101+64blit@users.noreply.github.com>
2023-09-12 08:40:24 -0700

    Cleared Main

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Xyz3D } from "./components/Xyz3D";
-import "./styles.css";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<Xyz3D />);
